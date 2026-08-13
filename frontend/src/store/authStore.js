f14e2bb96376b9db450728b48b78f5499ad2299f
diorpm06 <diorpm06@users.noreply.github.com>
2026-08-13 13:30:18 +0500

    fix: oq fon muammosi tuzatildi - dark theme default, inline theme script qoshildi

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -9,7 +9,7 @@ export const useAuthStore = create(
       role: null,
       fullName: null,
       userId: null,
-      theme: 'light',
+      theme: 'dark',
       setAuth: (data) =>
         set({
           accessToken: data.access_token,
